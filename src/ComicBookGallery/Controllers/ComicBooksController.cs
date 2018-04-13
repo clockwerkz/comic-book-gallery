dc9f2bc3f74f07bab8bf952a7647d53f2bee4a25
Carlos <clockwerkz@gmail.com>
2018-04-13 00:00:16 -0700

    Created a new Comic Book Detail View.

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,17 +11,7 @@ namespace ComicBookGallery.Controllers
 
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
-            {
-                return Redirect("/");
-                //return new RedirectResult("/");
-            }
-
-            return Content("Hello from the Comic Books Controller!");
-            /*return new ContentResult()
-            {
-                Content = "Hello from the Comic Books Controller!"
-            };*/
+            return View();
         }
     }
 }
