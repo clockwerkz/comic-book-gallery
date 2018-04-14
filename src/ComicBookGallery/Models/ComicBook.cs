118472861ed04321c76aff8423ded481fb10ade6
Carlos <clockwerkz@gmail.com>
2018-04-13 22:14:51 -0700

    Updated Comic Book Model to include two new Properties that are Get Only.

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -13,5 +13,23 @@ namespace ComicBookGallery.Models
         public string DescriptionHtml { get; set; } //Property will contain HTML Content, so we labeled the property name to reflect this.
         public Artist[] Artists { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber; 
+            }
+         }
+
+    // series-title-issuenumber.jpg
+    public string CoverImageFileName
+    {
+        get
+        {
+            return SeriesTitle.Replace(" ", "-")
+                .ToLower() + "-" + IssueNumber + ".jpg";
+        }
     }
+}
 }
