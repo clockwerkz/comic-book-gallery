6e0d288f61aedb38db3cb252b1e181f6f6a7658c
Carlos <clockwerkz@gmail.com>
2018-04-13 22:06:45 -0700

    Added ComicBook and Artist Models

---
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class ComicBook
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; } //Property will contain HTML Content, so we labeled the property name to reflect this.
+        public Artist[] Artists { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
