90709431ef69da93f5d86f6720c71f115441eb4a
Carlos <clockwerkz@gmail.com>
2018-04-15 23:23:44 -0700

    Updated Comic Books List view - Details index page

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -29,16 +29,17 @@ namespace ComicBookGallery.Data
             {
                 Id = 2,
                 SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                IssueNumber = 657,
+                DescriptionHtml = "<p><strong>FF: THREE TIE-IN.</strong> Spider-Man visits the FF for a very private wake--just for family.</p>",
                 Artists = new Artist[]
                 {
-                    new Artist() { Name = "Dan Slott", Role = "Script"},
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils"},
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks"},
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors"},
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters"},
-                }
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Marcos Martin", Role = "Pencils" },
+                    new Artist() { Name = "Marcos Martin", Role = "Inks" },
+                    new Artist() { Name = "Muntsa Vicente", Role = "Colors" },
+                    new Artist() { Name = "Joe Caramagna", Role = "Letters" }
+                },
+                Favorite = false
             },
             new ComicBook()
             {   
