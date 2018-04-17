9012e4de6e08c99d6cf702bc93ce2c68c27c18ea
Carlos <clockwerkz@gmail.com>
2018-04-16 22:09:23 -0700

    Added a Series Object to track the different comic book series with a detailed object vs just a string.

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -8,7 +8,7 @@ namespace ComicBookGallery.Models
     public class ComicBook
     {
         public int Id { get; set; }
-        public string SeriesTitle { get; set; }
+        public Series Series { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; } //Property will contain HTML Content, so we labeled the property name to reflect this.
         public Artist[] Artists { get; set; }
@@ -18,7 +18,15 @@ namespace ComicBookGallery.Models
         {
             get
             {
-                return SeriesTitle + " #" + IssueNumber; 
+                Series series = Series;
+                if (series != null)
+                {
+                    return Series.Title + " #" + IssueNumber;
+                }
+                else
+                {
+                    return null;
+                }
             }
          }
 
@@ -27,8 +35,16 @@ namespace ComicBookGallery.Models
     {
         get
         {
-            return SeriesTitle.Replace(" ", "-")
-                .ToLower() + "-" + IssueNumber + ".jpg";
+                Series series = Series;
+                if (series != null)
+                {
+                    return Series.Title.Replace(" ", "-")
+                        .ToLower() + "-" + IssueNumber + ".jpg";
+                }
+                else
+                {
+                    return null;
+                }       
         }
     }
 }
